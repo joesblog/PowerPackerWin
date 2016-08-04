98bdb6a810f8e8f4d648c86e2efe096a3f7c8811
Joe <joe@joesblog.me.uk>
2016-08-04 13:00:23 +0100

    Add files via upload

---
+++ PowerPackerExample/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PowerPackerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbOpenResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otsmOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.otsmSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbOpenResult
+            // 
+            this.tbOpenResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOpenResult.Location = new System.Drawing.Point(12, 59);
+            this.tbOpenResult.Multiline = true;
+            this.tbOpenResult.Name = "tbOpenResult";
+            this.tbOpenResult.Size = new System.Drawing.Size(694, 286);
+            this.tbOpenResult.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(718, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otsmOpen,
+            this.otsmSaveAs});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // otsmOpen
+            // 
+            this.otsmOpen.Name = "otsmOpen";
+            this.otsmOpen.Size = new System.Drawing.Size(152, 22);
+            this.otsmOpen.Text = "Open";
+            this.otsmOpen.Click += new System.EventHandler(this.otsmOpen_Click);
+            // 
+            // otsmSaveAs
+            // 
+            this.otsmSaveAs.Name = "otsmSaveAs";
+            this.otsmSaveAs.Size = new System.Drawing.Size(152, 22);
+            this.otsmSaveAs.Text = "Save As..";
+            this.otsmSaveAs.Click += new System.EventHandler(this.otsmSaveAs_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.creditsToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // creditsToolStripMenuItem
+            // 
+            this.creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            this.creditsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.creditsToolStripMenuItem.Text = "Credits";
+            this.creditsToolStripMenuItem.Click += new System.EventHandler(this.creditsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 357);
+            this.Controls.Add(this.tbOpenResult);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PowerPacker Example";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOpenResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otsmOpen;
+        private System.Windows.Forms.ToolStripMenuItem otsmSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creditsToolStripMenuItem;
+    }
+}
+
---
+++ PowerPackerExample/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace PowerPackerExample
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("PowerPacker32.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern void dumpToFile(string source, string destination);
+
+
+        [DllImport("PowerPacker32.dll", SetLastError = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr convert(string source);
+
+        string pathOfCurrentFile { get; set; }
+        string pathForSaveFile { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void otsmOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FileOk += Ofd_FileOk;
+            ofd.ShowDialog();
+
+ 
+
+        }
+
+        private void Ofd_FileOk(object sender, CancelEventArgs e)
+        {
+            if (((OpenFileDialog)sender).CheckFileExists)
+            {
+                pathOfCurrentFile = ((OpenFileDialog)sender).FileName;
+                openPPFile();
+            }
+            else
+            {
+                MessageBox.Show("No file selected");
+            }
+
+        }
+
+        private void otsmSaveAs_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileOk += Sfd_FileOk;
+
+            sfd.ShowDialog();
+
+
+      
+
+
+        }
+
+
+      private  void openPPFile()
+        {
+            try
+            {
+                IntPtr pt3r = new IntPtr();
+                pt3r = convert(pathOfCurrentFile);
+                tbOpenResult.Text = Marshal.PtrToStringAnsi(pt3r).Replace("\r\n", "\n");
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Something failed in opening this file, are you sure its a PP20?");
+            }
+        }
+        private void Sfd_FileOk(object sender, CancelEventArgs e)
+        {
+         
+                pathForSaveFile = ((SaveFileDialog)sender).FileName;
+
+            if (pathOfCurrentFile != null && pathOfCurrentFile != "")
+            {
+                dumpToFile(pathOfCurrentFile, pathForSaveFile);
+            }
+        }
+
+        private void creditsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmAbout fra = new frmAbout();
+            fra.Show();
+        }
+    }
+      
+
+
+}
